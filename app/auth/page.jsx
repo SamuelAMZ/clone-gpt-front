5b42a077a5aa29eb4cdd8154a6a7f3f8420a8429
samueldevpro <samueldevpro09@gmail.com>
2023-06-26 18:37:39 +0000

    redirect

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -97,13 +97,13 @@ const Auth = () => {
   };
 
   const [user, loading] = useAuthState(auth);
-  // useEffect(() => {
-  //   if (user) {
-  //     console.log(user);
-  //     // redirect to dash
-  //     window.location.href = `${process.env.NEXT_PUBLIC_CLIENT}/disc/643119b7087e7292142acc3b`;
-  //   }
-  // }, [user]);
+  useEffect(() => {
+    if (user) {
+      console.log(user);
+      // redirect to dash
+      window.location.href = `${process.env.NEXT_PUBLIC_CLIENT}/disc/643119b7087e7292142acc3b`;
+    }
+  }, [user]);
 
   return (
     <div className="clonegpt-login-container">
